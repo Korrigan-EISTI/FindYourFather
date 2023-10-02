d1c443cf92e0a1a42cb61db9ebcb99a473f9788d
Sinol95 <la.laguet95@gmail.com>
2023-10-02 14:04:07 +0200

    Init

---
+++ src/main/java/com/dreamteam/findyourfather/FindYourFatherApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.findyourfather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourFatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourFatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamteam/findyourfather/FindYourFatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.findyourfather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourFatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
