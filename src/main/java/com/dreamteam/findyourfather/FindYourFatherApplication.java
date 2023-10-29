b18da89c885ed756fa79b5447a83c1b21ecdf5af
Sinol95 <la.laguet95@gmail.com>
2023-10-29 12:46:58 +0100

    Build updates

--- src/main/java/com/dreamteam/findyourfather/FindYourFatherApplication.java
+++ src/main/java/com/dreamteam/findyourfather/FindYourFatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FindyourfatherApplication {
+public class FindYourFatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FindyourfatherApplication.class, args);
+		SpringApplication.run(FindYourFatherApplication.class, args);
 	}
 
 }
--- src/test/java/com/dreamteam/findyourfather/FindYourFatherApplicationTests.java
+++ src/test/java/com/dreamteam/findyourfather/FindYourFatherApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FindyourfatherApplicationTests {
+class FindYourFatherApplicationTests {
 
 	@Test
 	void contextLoads() {
