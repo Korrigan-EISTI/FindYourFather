e9481a1f0ae279f48935cbefb318d8b71f2afb50
Sinol95 <la.laguet95@gmail.com>
2023-10-31 16:14:59 +0100

    Login CSS upgraded + Register field

--- frontend/src/app/components/auth/auth.component.ts
+++ frontend/src/app/components/auth/auth.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+	selector: 'app-auth',
+	templateUrl: './auth.component.html',
+	styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-  email: string = '';
-  password: string = '';
+	loginEmail: string = '';
+	loginPassword: string = '';
 
-  onSubmit() {
-    console.log('Formulaire soumis :', this.email, this.password);
-  }
+	registrationEmail: string = '';
+	registrationPassword: string = '';
+	registrationSsn: string = '';
+	registrationLastname: string = '';
+	registrationFirstname: string = '';
+	registrationBirthdate: string = '';
+	registrationNationality: string = '';
+	registrationGender: string = '';
+
+	onLoginSubmit() {
+		console.log('Login Form Submitted:', this.loginEmail, this.loginPassword);
+	}
+
+	onRegisterSubmit() {
+		console.log(
+			'Registration Form Submitted:',
+			this.registrationEmail,
+			this.registrationPassword,
+			this.registrationSsn,
+			this.registrationLastname,
+			this.registrationFirstname,
+			this.registrationBirthdate,
+			this.registrationNationality,
+			this.registrationGender
+		);
+	}
 }
