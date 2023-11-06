0a9dd18273a83b4b831243fad473f6f9379acc52
Sinol95 <la.laguet95@gmail.com>
2023-11-06 08:53:51 +0100

    GO JS

--- frontend/src/app/components/family-tree/family-tree.component.ts
+++ frontend/src/app/components/family-tree/family-tree.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as go from 'gojs';
 
 @Component({
   selector: 'app-family-tree',
   templateUrl: './family-tree.component.html',
   styleUrls: ['./family-tree.component.css']
 })
-export class FamilyTreeComponent {
+export class FamilyTreeComponent implements OnInit {
+  diagram = new go.Diagram();
+
+  constructor() { }
+
+  ngOnInit() {
+  	this.diagram = new go.Diagram('family-tree-diagram');
+  }
 
 }
