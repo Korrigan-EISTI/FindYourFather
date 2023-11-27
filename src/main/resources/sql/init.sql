b70cee70e388bb638144770b8959d95f9520d03a
Sinol95 <la.laguet95@gmail.com>
2023-11-27 10:23:20 +0100

    Updated init.sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,10 +1,19 @@
---Personnes
-INSERT INTO public.personne (genre, naissance, id, numero_secu, nom, prenom) VALUES (1, '720', 4, 2, 'OUI_GRAND_PERE', 'OUI_GRAND_PERE');
-INSERT INTO public.personne (genre, naissance, id, numero_secu, nom, prenom, pere) VALUES (0, '1200', 2, 2, 'OUI_PERE', 'OUI_PERE', 4);
-INSERT INTO public.personne (genre, naissance, id, numero_secu, nom, prenom) VALUES (1, '720', 3, 2, 'OUI_MERE', 'OUI_MERE');
-INSERT INTO public.personne (genre, naissance, id, numero_secu, nom, prenom, pere) VALUES (1, '720', 5, 2, 'OUI_ONCLE', 'OUI_ONCLE', 4);
-INSERT INTO public.personne (genre, naissance, id, numero_secu, nom, prenom, pere, mere) VALUES (1, '720', 1, 2, 'OUI', 'OUI', 2, 3);
+-- Supposons qu'il existe déjà une table nommée "personne"
 
---User
-INSERT INTO public.utilisateur (id, email, id_personne, mdp, visibility_level) VALUES (1, 'test@test.com', 1, 'oui', 0)
+-- Insertion de données d'exemple pour une famille
+INSERT INTO personne (id, numero_secu, nom, prenom, date_deces, nationalite, genre, naissance, pere, mere)
+VALUES
+  -- Enfant
+  (1, 123456789, 'Doe', 'John', NULL, 'French', 1, '01/01/1990', NULL, NULL),
+  
+  -- Parents
+  (2, 111111111, 'Doe', 'Peter', NULL, 'French', 1, '01/01/1965', NULL, NULL), -- Père
+  (3, 222222222, 'Doe', 'Mary', NULL, 'French', 0, '01/01/1970', NULL, NULL), -- Mère
 
+  -- Grands-parents
+  (4, 333333333, 'Doe', 'George', NULL, 'French', 1, '01/01/1940', NULL, NULL), -- Grand-père paternel
+  (5, 444444444, 'Doe', 'Alice', NULL, 'French', 0, '01/01/1945', NULL, NULL); -- Grand-mère paternelle
+
+-- Liaison des individus dans la famille
+UPDATE personne SET pere = 2, mere = 3 WHERE id = 1; -- Enfant lié aux parents
+UPDATE personne SET pere = 4, mere = 5 WHERE id IN (2, 3); -- Parents liés aux grands-parents
